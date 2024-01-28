a3decf06dc576a7fe507071e24220e75a420469d
Khauneesh_Saigal <Khauneesh_Saigal@dell.com>
2024-01-29 01:27:54 +0530

    made changes to RAG code

--- qa/extract_pdf.py
+++ qa/extract_pdf.py
@@ -1,41 +1,92 @@
-import docx2txt
-from PyPDF2 import PdfReader
-
-from dsx_genai import Embeddings
 import json
-import ast
+import os
+import time
+import pandas as pd
+import pickle
+import numpy as np 
+from dotenv import load_dotenv
+from functools import wraps
 
-from utils import create_embeddings_for_text
-from config import *
+from openai import OpenAI
+import tiktoken
+import pdfplumber
+from utils import timed
+import time
 
 
-from langchain.embeddings import OpenAIEmbeddings
-model = OpenAIEmbeddings(openai_api_key="your-api-key")
+load_dotenv()
 
-#filename='sandra_wise.pdf'
-filename = 'Boeing-2022-Annual-Report.pdf'
 
-reader = PdfReader(filename)
-extracted_text = ""
-for page in reader.pages:
-    extracted_text += page.extract_text()
-print('done')
+client = OpenAI()
+pdf_path = 'Boeing-2022-Annual-Report.pdf'
+encoding = tiktoken.encoding_for_model("text-embedding-ada-002")
 
-# with open("./acc_clinton.txt") as f:
-#     txt = f.read()
-# print('done')
+chunk_size = 300
 
-text_plus_embeddings = create_embeddings_for_text(model, extracted_text)
-#print(text_plus_embeddings)
-docs=[]
-for i, (text_chunk, embedding) in enumerate(text_plus_embeddings):
-    doc ={}
-    doc['filename']='boing_10k'
-    doc['chunk_index']=i
-    doc['text_chunk']=text_chunk
-    doc['embeddings']=embedding
-    docs.append(doc)
-    print('done chunking')
 
-with open('docs_boeing.json','w') as docs_json:
-    json.dump(docs, docs_json)
+@timed
+def extract_pdf(pdf_path):
+    all_text = ''
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                all_text += text + '\n'
+    return all_text
+
+
+def calc_tokens(text: str) -> int:
+    return len(encoding.encode(text))
+
+
+@timed
+def chunker(text):
+    window = []
+    window_size = 0
+    for line in text.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+        line_size = calc_tokens(line)
+        # grow window until largest possible chunk
+        if window_size + line_size < chunk_size:
+            window.append(line)
+            window_size += line_size + 1
+        # reset window
+        else:
+            if window:
+                yield "\n".join(window)
+            window = [line]
+            window_size = line_size
+    # return the leftover
+    if window:
+        yield "\n".join(window)
+
+@timed
+def get_embed(text):
+    return client.embeddings.create(input = [text],  model = 'text-embedding-3-small').data[0].embedding
+
+
+
+@timed
+def run_main(pdf_path):
+
+    
+    text_content = extract_pdf(pdf_path)
+    print('pdf extracted')
+    chunks = list(chunker(text_content))
+    embeddings_l = list(map(get_embed, chunks))
+    print('embeddings generated')
+
+    embeddings = np.array(embeddings_l)
+    embeddings = embeddings / np.sqrt((embeddings**2).sum(1, keepdims=True))
+
+    return chunks , embeddings
+
+chunks , embeddings = run_main(pdf_path)
+
+with open('boeing.pkl', 'wb') as file:
+    pickle.dump((chunks, embeddings), file)
+
+
+
--- qa/question-answer.py
+++ qa/question-answer.py
@@ -1,66 +1,114 @@
-import json
-with open('docs_boeing.json','r') as docs_json:
-    docs = json.load(docs_json)
-
-
-import ast
+import pickle
+from openai import OpenAI
+from dotenv import load_dotenv
 import numpy as np
 
-import os
-embeddings = []
-for doc in docs:
-    embeddings.append(ast.literal_eval(doc['embeddings']))
+load_dotenv()
 
-embeddings = np.array(embeddings)
-embeddings = embeddings / np.sqrt((embeddings**2).sum(1, keepdims=True)) # L2 normalize the rows, as is common
+client = OpenAI()
 
 
 
+with open('boeing.pkl', 'rb') as file:
+    texts, embeddings = pickle.load(file)
 
-from langchain.embeddings import OpenAIEmbeddings
-model = OpenAIEmbeddings(openai_api_key="your-api-key")
-
-
-sentence = "What are future business plans of boeing ?"
+print(len(texts), embeddings.shape)
 
 
-query = model.encode(sentence)
+def get_embed(text):
+    return client.embeddings.create(input = [text],  model = 'text-embedding-3-small').data[0].embedding
 
+
+#question = "What was total revenue of boeing in 2022, 2021, 2020 respectively?"
+#question = 'Tell me about top 5 objectives of Boeing in bullet points'
+#question = "Tell me about IT infra of boeing"
+question = 'Tell me about subsidiaries or segments for  boeing company?'
+#question = ' Tell me about revenue of Defense, Space & Security (BDS)'
+#question = 'Tell me about backlogs of boeing in 2022 , 2021 , 2020 , respectively'ArithmeticError
+
+query = get_embed(question)
+query = np.array(query)
 query = query/ np.sqrt((query**2).sum())
-
-# Wired: use an SVM
-
-
-from sklearn import svm
-
-# create the "Dataset"
-x = np.concatenate([query[None,...], embeddings]) # x is (num_docs, embeddings) array, with query now as the first row
-y = np.zeros(len(x))
-y[0] = 1 # we have a single positive example, mark it as such
-
-# train our (Exemplar) SVM
-# docs: https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
-
-clf = svm.LinearSVC(class_weight='balanced', verbose=False, max_iter=10000, tol=1e-6, C=0.6)
-clf.fit(x, y) # train
-
-# infer on whatever data you wish, e.g. the original data
-similarities = clf.decision_function(x)
+similarities = embeddings.dot(np.transpose(query))
 sorted_ix = np.argsort(-similarities)
-for k in sorted_ix[:10]:
-  print(f"row {k}, similarity {similarities[k]}")
-
-
-
-
-
-#******************************************************svm approach*************************************************************************
 
 
 files_string = ''
-for k in sorted_ix[1:3]:
-    files_string=files_string+'###\n'
-    files_string=files_string+docs[k-1]['text_chunk']+'\n'
+files_string=files_string+'###\n' + texts[sorted_ix[0]] +'###\n' + texts[sorted_ix[2]] +'###\n' + texts[sorted_ix[4]] + texts[sorted_ix[3]] + texts[sorted_ix[1]]
+#print(files_string)
+
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant that answers questions based on the context provided."},
+    {"role": "user", "content": f'''
+    Provide me the answer to the given question: {question} based on the text provided below
+    {files_string}.
+    Don't give any answer if the given text/context doesn't have necessary information.
+    Report numbers if any in millions.
+    '''}
+  ]
+)
+
+print(completion.choices[0].message.content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Wired: use an SVM
+
+
+# from sklearn import svm
+
+# # create the "Dataset"
+# x = np.concatenate([query[None,...], embeddings]) # x is (num_docs, embeddings) array, with query now as the first row
+# y = np.zeros(len(x))
+# y[0] = 1 # we have a single positive example, mark it as such
+
+# # train our (Exemplar) SVM
+# # docs: https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
+
+# clf = svm.LinearSVC(class_weight='balanced', verbose=False, max_iter=10000, tol=1e-6, C=0.6)
+# clf.fit(x, y) # train
+
+# # infer on whatever data you wish, e.g. the original data
+# similarities = clf.decision_function(x)
+# sorted_ix = np.argsort(-similarities)
+# for k in sorted_ix[:10]:
+#   print(f"row {k}, similarity {similarities[k]}")
+
+
+
+
+
+# #******************************************************svm approach*************************************************************************
+
+
+# files_string = ''
+# for k in sorted_ix[1:3]:
+#     files_string=files_string+'###\n'
+#     files_string=files_string+docs[k-1]['text_chunk']+'\n'
 
 # comment above part and uncomment below part if you want to just use cosine similarity. 
 # cosine similarity is much more scalable while SVM yields better result on diverse data.
@@ -77,33 +125,3 @@ for k in sorted_ix[1:3]:
 # sorted_ix = np.argsort(-similarities)
 
 
-# files_string = ''
-# for k in sorted_ix[0:2]:
-#     files_string=files_string+'###\n'
-#     files_string=files_string+docs[k-1]['text_chunk']+'\n'
-
-
-
-
-prompt =f'''
-Human: If I give you some paragraphs, can you use them to answer the question {sentence} ?
-Assistant: Sure. I'll keep my answer precise.
-Human: Remember to only use information provided in paragraphs. Don't answer questions you are not sure about.
-Assistant: Noted. Please provide me the paragraphs.
-
-Human: Here they are {files_string}
-
-Assistant:
-'''
-#
-print(prompt)
-
-#print(sentence, '\n')
-
-
-from langchain.llms import OpenAI
-
-llm = OpenAI(openai_api_key="...")
-
-
-print(llm(prompt))
